567854b4fb920c4f21861415fb39c024bb50a81e
profgstv <gustavodal@prof.educacao.sp.gov.br>
2025-10-09 14:04:47 -0300

    Criação de um sorteador para alterar a ordem dos cards a cada inicialização/atualização da página.

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const projectsList = {
-        projectName: ["Recomendador de Digimons", "Bem vindos ao Egito", "Monalisa no P5.js", "Jogo da Nave", "Sorteador de Palavras", "Jogo do Pong no P5.js", "Rádio Inovação CIEBP", "Simulador de Coordenadas", "Scratch Giratório no P5.js", "Digimon Card Battle", "Jogo do Pong no Scratch", "Salve o Planeta no Scratch", "Carrinho de Drift", "Carrinho de Eixo"],
-        src: ["./img/proj-p5-digimon.png", "./img/proj-p5-egito.png", "./img/proj-p5-monalisa.png", "./img/proj-p5-nave-desvio.png", "./img/proj-p5-palavra.png", "./img/proj-p5-pong.png", "./img/proj-p5-radio.png", "./img/proj-p5-sim-coord-controle-drift.png", "./img/proj-p5-spinning-scratch.png", "./img/proj-scratch-digimon-card-battle.png", "./img/proj-scratch-pong.png", "./img/proj-scratch-salve-o-planeta.png", "./img/proj-tinkercad-carrinho-drift.png", "./img/proj-tinkercad-carrinho-eixo.png"],
-        alt: ["Imagem do Recomendador de Digimon no P5.js.", "Imagem do Projeto Bem vindo ao Egito no P5.js.", "Imagem do Projeto da Mona Lisa no P5.js.", "Imagem do Jogo da Nave no P5.js.", "Imagem do Sorteador de Palavra no P5.js.", "Imagem do Jogo do Pong no P5.js.", "Imagem do Projeto Rádio Inovação CIEBP.", "Imagem do Simulador de Coordenadas para acelerômetro no P5.js.", "Imagem do Scratch Giratório no P5.js.", "Imagem Jogo Digimon Card Battle no Scratch.", "Imagem do Jogo do Pong no Scratch.", "Imagem do Jogo Salve o Planeta.", "Imagem do projeto do carrinho de Drift.", "Imagem do projeto do carrinho com eixo.", "Imagem do Recomendador de Digimon no P5.js."],
-        modalID: ["modal001", "modal002", "modal003", "modal004", "modal005", "modal006", "modal007", "modal008", "modal009", "modal010", "modal011", "modal012", "modal013", "modal014"],
-        basicDescription: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ","Carrinho movido por meio de Drift com 4 motores DC.", "Carrinho feito com materiais não estruturados para utilização com eixo de servo motor e movimentação com motor DC.", " "],
-        description0: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ","Carrinho motorizado movido por meio de Drift. Possui espaço para acoplagem da Shield Robotbit da Kittenbot para Micro:bit, Sensores de Linha com Infravermelho modelo Grove, Sensores Ultrassônicos v2.0 e Sensores RGB TCS3200.", "Carrinho de eixo, montado sobre uma base retangular de acrílico reutilizado, com rodas de tampinhas de leite brancas, motor DC para movimentação e um eixo central movido por um servo motor construído com um canudo plástico e um palito de bambu que permite a rotação.", " "],
-        description1: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ","Pode ser montado e desmontado de diversas formas, utilizando programações também diversas.", "Sobre a base podem ser utilizados diversos microcontroladores. Nesse projeto é utilizado o Micro:bit com a Shield Robotbit da Kittenbot e configurado um controle remoto com outro Micro:bit por rádio frequência e acelerômetro.", " "],
-        description2: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        link0: ["https://editor.p5js.org/gustavodal/sketches/kz7fL2tDb", "https://editor.p5js.org/gustavodal/sketches/qTJDQoaKi", "https://editor.p5js.org/gustavodal/sketches/QXvpZovlZ", "https://editor.p5js.org/gustavodal/sketches/6RbxdTuLY", "https://editor.p5js.org/gustavodal/sketches/OSxJst87c", "https://editor.p5js.org/gustavodal/sketches/-HbSqY4om", "https://editor.p5js.org/gustavodal/sketches/08-tT-q5H", "https://editor.p5js.org/gustavodal/sketches/ZtqfcN_1j", "https://editor.p5js.org/gustavodal/sketches/NLQ8hj56q", "https://scratch.mit.edu/projects/1075392584/", "https://scratch.mit.edu/projects/1059549889/", "https://scratch.mit.edu/projects/1162238548/", "https://www.tinkercad.com/things/huxaQpIZsmd-robo-drift-shield-robotbit-microbit", "https://www.tinkercad.com/things/6tjgtFD3odC-carrinho-de-material-nao-estruturado-com-eixo", "https://editor.p5js.org/gustavodal/sketches/kz7fL2tDb"],
-        linkName0: ["Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no Scratch", "Projeto no Scratch", "Projeto no Scratch","Protótipo no Tinkercad", "Protótipo no Tinkercad", "Projeto no P5.js"],
-        link1: [" ", " ", " ", " ", " ", " ", " ","https://editor.p5js.org/gustavodal/sketches/_vy5DUZ8o", "https://scratch.mit.edu/projects/1140165261/", " ", " ", " ","https://profgstv.github.io/microbit-carrinho-de-drift-com-controle-analgico/", "https://github.com/profgstv/microbit-carrinho-de-controle-analogico-com-eixo", " "],
-        linkName1: [" ", " ", " ", " ", " ", " ", " ", " ","Projeto no Scratch", " ", " ", " ","Programação com Controle Remoto Analógico", "Programação com Controle por Acelerômetro", " "],
-        link2: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        linkName2: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        link3: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        linkName3: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        link4: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        linkName4: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    projectName: ["Recomendador de Digimons", "Bem vindos ao Egito", "Monalisa no P5.js", "Jogo da Nave", "Sorteador de Palavras", "Jogo do Pong no P5.js", "Rádio Inovação CIEBP", "Simulador de Coordenadas", "Scratch Giratório no P5.js", "Digimon Card Battle", "Jogo do Pong no Scratch", "Salve o Planeta no Scratch", "Carrinho de Drift", "Carrinho de Eixo"],
+    src: ["./img/proj-p5-digimon.png", "./img/proj-p5-egito.png", "./img/proj-p5-monalisa.png", "./img/proj-p5-nave-desvio.png", "./img/proj-p5-palavra.png", "./img/proj-p5-pong.png", "./img/proj-p5-radio.png", "./img/proj-p5-sim-coord-controle-drift.png", "./img/proj-p5-spinning-scratch.png", "./img/proj-scratch-digimon-card-battle.png", "./img/proj-scratch-pong.png", "./img/proj-scratch-salve-o-planeta.png", "./img/proj-tinkercad-carrinho-drift.png", "./img/proj-tinkercad-carrinho-eixo.png"],
+    alt: ["Imagem do Recomendador de Digimon no P5.js.", "Imagem do Projeto Bem vindo ao Egito no P5.js.", "Imagem do Projeto da Mona Lisa no P5.js.", "Imagem do Jogo da Nave no P5.js.", "Imagem do Sorteador de Palavra no P5.js.", "Imagem do Jogo do Pong no P5.js.", "Imagem do Projeto Rádio Inovação CIEBP.", "Imagem do Simulador de Coordenadas para acelerômetro no P5.js.", "Imagem do Scratch Giratório no P5.js.", "Imagem Jogo Digimon Card Battle no Scratch.", "Imagem do Jogo do Pong no Scratch.", "Imagem do Jogo Salve o Planeta.", "Imagem do projeto do carrinho de Drift.", "Imagem do projeto do carrinho com eixo.", "Imagem do Recomendador de Digimon no P5.js."],
+    modalID: ["modal001", "modal002", "modal003", "modal004", "modal005", "modal006", "modal007", "modal008", "modal009", "modal010", "modal011", "modal012", "modal013", "modal014"],
+    basicDescription: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ","Carrinho movido por meio de Drift com 4 motores DC.", "Carrinho feito com materiais não estruturados para utilização com eixo de servo motor e movimentação com motor DC.", " "],
+    description0: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ","Carrinho motorizado movido por meio de Drift. Possui espaço para acoplagem da Shield Robotbit da Kittenbot para Micro:bit, Sensores de Linha com Infravermelho modelo Grove, Sensores Ultrassônicos v2.0 e Sensores RGB TCS3200.", "Carrinho de eixo, montado sobre uma base retangular de acrílico reutilizado, com rodas de tampinhas de leite brancas, motor DC para movimentação e um eixo central movido por um servo motor construído com um canudo plástico e um palito de bambu que permite a rotação.", " "],
+    description1: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ","Pode ser montado e desmontado de diversas formas, utilizando programações também diversas.", "Sobre a base podem ser utilizados diversos microcontroladores. Nesse projeto é utilizado o Micro:bit com a Shield Robotbit da Kittenbot e configurado um controle remoto com outro Micro:bit por rádio frequência e acelerômetro.", " "],
+    description2: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    link0: ["https://editor.p5js.org/gustavodal/sketches/kz7fL2tDb", "https://editor.p5js.org/gustavodal/sketches/qTJDQoaKi", "https://editor.p5js.org/gustavodal/sketches/QXvpZovlZ", "https://editor.p5js.org/gustavodal/sketches/6RbxdTuLY", "https://editor.p5js.org/gustavodal/sketches/OSxJst87c", "https://editor.p5js.org/gustavodal/sketches/-HbSqY4om", "https://editor.p5js.org/gustavodal/sketches/08-tT-q5H", "https://editor.p5js.org/gustavodal/sketches/ZtqfcN_1j", "https://editor.p5js.org/gustavodal/sketches/NLQ8hj56q", "https://scratch.mit.edu/projects/1075392584/", "https://scratch.mit.edu/projects/1059549889/", "https://scratch.mit.edu/projects/1162238548/", "https://www.tinkercad.com/things/huxaQpIZsmd-robo-drift-shield-robotbit-microbit", "https://www.tinkercad.com/things/6tjgtFD3odC-carrinho-de-material-nao-estruturado-com-eixo", "https://editor.p5js.org/gustavodal/sketches/kz7fL2tDb"],
+    linkName0: ["Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no P5.js", "Projeto no Scratch", "Projeto no Scratch", "Projeto no Scratch","Protótipo no Tinkercad", "Protótipo no Tinkercad", "Projeto no P5.js"],
+    link1: [" ", " ", " ", " ", " ", " ", " ","https://editor.p5js.org/gustavodal/sketches/_vy5DUZ8o", "https://scratch.mit.edu/projects/1140165261/", " ", " ", " ","https://profgstv.github.io/microbit-carrinho-de-drift-com-controle-analgico/", "https://github.com/profgstv/microbit-carrinho-de-controle-analogico-com-eixo", " "],
+    linkName1: [" ", " ", " ", " ", " ", " ", " ", " ","Projeto no Scratch", " ", " ", " ","Programação com Controle Remoto Analógico", "Programação com Controle por Acelerômetro", " "],
+    link2: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    linkName2: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    link3: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    linkName3: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    link4: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+    linkName4: [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 }
 
-function createCard(i) {
+const parentElement = document.body;
+const referenceElement = document.getElementById("reference");
+
+const randomizedProjects = projectsList.projectName.toSorted(() => Math.random() - 0.5);
+
+function createCard(a) {
+    let i = projectsList.projectName.indexOf(randomizedProjects[a]);
+
     let cards = document.getElementById("projectsCards");
     let cardDiv0 = document.createElement("div");
     let cardDiv1 = document.createElement("div");
@@ -63,10 +70,10 @@ function createCard(i) {
     cardButton.innerHTML = "Detalhes";
 }
 
-const parentElement = document.body;
-const referenceElement = document.getElementById("reference");
 
-function createModal(i) {
+function createModal(a) {
+    let i = projectsList.projectName.indexOf(randomizedProjects[a]);
+
     let modalDiv0 = document.createElement("div");
     let modalDiv1 = document.createElement("div");
     let modalDiv2 = document.createElement("div");
@@ -157,8 +164,7 @@ function createModal(i) {
 
 }
 
-for(let i in projectsList.projectName) {
+for(let i in projectsList.projectName) {;
     createCard(i);
-    
     createModal(i);
 }
